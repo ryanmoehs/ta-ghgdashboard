b5e0ff2452429f2632875a9567ea1bfea749658b
Ryan Muhammad <masryandepok@gmail.com>
2025-06-02 10:06:04 +0700

    fix: edit structure failure

--- database/seeders/FuelPropertiesSeeder.php
+++ database/seeders/FuelPropertiesSeeder.php
@@ -13,31 +13,32 @@ class FuelPropertiesSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('fuel_properties')->insert(
-            [
-                'fuel_type' => 'Solar CN48',
-                'unit' => 'liter',
-                'conversion_factor' => 0.0000386,
-                'co2_factor' => 74100,
-                'ch4_factor' => 3,
-                'n2o_factor' => 0.6
-            ],
-            [
-                'fuel_type' => 'Batubara Antrasit',
-                'unit' => 'ton',
-                'conversion_factor' => 0.024,
-                'co2_factor' => 98300,
-                'ch4_factor' => 1,
-                'n2o_factor' => 1.5
-            ],
-            [
-                'fuel_type' => 'Batubara Sub-bituminous',
-                'unit' => 'ton',
-                'conversion_factor' => 0.024,
-                'co2_factor' => 96100,
-                'ch4_factor' => 1,
-                'n2o_factor' => 1.5
+        DB::table('fuel_properties')->insert([
+                [
+                    'fuel_type' => 'Solar CN48',
+                    'unit' => 'liter',
+                    'conversion_factor' => 0.0000386,
+                    'co2_factor' => 74100,
+                    'ch4_factor' => 3,
+                    'n2o_factor' => 0.6
+                ],
+                [
+                    'fuel_type' => 'Batubara Antrasit',
+                    'unit' => 'ton',
+                    'conversion_factor' => 0.024,
+                    'co2_factor' => 98300,
+                    'ch4_factor' => 1,
+                    'n2o_factor' => 1.5
+                ],
+                [
+                    'fuel_type' => 'Batubara Sub-bituminous',
+                    'unit' => 'ton',
+                    'conversion_factor' => 0.024,
+                    'co2_factor' => 96100,
+                    'ch4_factor' => 1,
+                    'n2o_factor' => 1.5
+                ]
             ]
-        );
-    }
+            );
+        }
 }
