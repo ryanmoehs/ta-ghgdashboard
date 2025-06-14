e0487fd9bdd6913378f1a63282b6488c9c8dc1c5
Ryan Muhammad <masryandepok@gmail.com>
2025-06-14 12:21:51 +0700

    feat: Memperbarui display data pada Dashboard

--- resources/views/sensor/index.blade.php
+++ resources/views/sensor/index.blade.php
@@ -58,6 +58,8 @@
                                                 @method('DELETE')
                                                 <button type="submit" class="text-red-600 hover:text-red-900">Delete</button>
                                             </form>
+                                            |
+                                            <a href="#">Maintenance</a>
                                         </td>
                                     </tr>
                                 @endforeach
